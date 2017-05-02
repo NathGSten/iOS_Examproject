53c17d2df473f55d20d1cdad524b60477638f7cf
Nathalie Stenhammer <nathalie_g_sten@hotmail.com>
2017-05-02 14:00:35 +0200

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Examproject
+//
+//  Created by Nathalie Stenhammer on 02/05/2017.
+//  Copyright © 2017 Nathalie Stenhammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
